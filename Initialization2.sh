1fc784daf5ae26aeed3357229bd955713dfb7544
dominickyeo <dominickyeo@outlook.com>
2025-03-21 07:22:32 -0700

    Update Initialization2.sh

--- Initialization2.sh
+++ Initialization2.sh
@@ -1,3 +1,5 @@
+sudo apt install -y nvidia-container-toolkit
+sudo systemctl restart docker
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 sudo python3 install_gpu_driver.py
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
