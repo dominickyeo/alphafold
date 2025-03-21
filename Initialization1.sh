b715ff377edd9a4786dcc56f793380bcc6e31cde
dominickyeo <dominickyeo@outlook.com>
2025-03-21 09:23:34 -0700

    Update Initialization1.sh

--- Initialization1.sh
+++ Initialization1.sh
@@ -1,6 +1,6 @@
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl 
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -11,7 +11,8 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-#sudo groupadd docker
+sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
