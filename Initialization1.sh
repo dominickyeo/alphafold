feca513b5ecfe88feb7ad9d0f4eabaf682e3ac0c
dominickyeo <dominickyeo@outlook.com>
2025-03-21 11:47:58 -0700

    Update Initialization1.sh

--- Initialization1.sh
+++ Initialization1.sh
@@ -13,6 +13,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+docker run hello-world
